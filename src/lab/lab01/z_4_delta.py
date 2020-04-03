963a454fb1d22d28d06a8ab4c1a251baa0876197
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-04-03 15:12:53 +0200

    delta

---
+++ src/lab/lab01/z_4_delta.py
@@ -0,0 +1,25 @@
+from math import sqrt
+a = 1
+b = 0
+c = 1
+
+if a == 0:
+    if b == 0:
+        if c == 0:
+            print('Duzo')
+        else:
+            print("Brak")
+    else:
+        print('x = ', (-c)/(b*1.0))
+else:
+    d = b * b - 4 * a * c
+    if d > 0:
+        dd = sqrt(d)
+        print('x1 = ', (-b - dd)/(2*a))
+        print('x2 = ', (-b + dd)/(2*a))
+    elif d == 0:
+        print('x = ', (-b)/(2.0*a))
+    else:
+        dd = sqrt(-d)*1j
+        print('x1 = ', (-b - dd)/(2*a))
+        print('x2 = ', (-b + dd)/(2*a))
