f1dc061bb7ed1611edcd82d91fbeb559dfb2d172
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-04-03 17:02:29 +0200

    macierze

--- src/lab/lab01/z_7_macierze.py
+++ src/lab/lab01/z_7_macierze.py
@@ -1,11 +1,28 @@
+from random import random
 
-m1 = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-m2 = [[1, 2, 3], [4, 5, 1], [2, 3, 4]]
+N = 2
 
-wynik = [[0 for y in range(3)] for x in range(3)]
+m1 = [
+        [(1 if i == j else 0) for j in range(N)]
+        for i in range(N)
+     ]
+m2 = [[int(random() * 100) for y in range(N)] for x in range(N)]
 
-#
-#
+print("m1:")
+for row in m1:
+    print(row)
+
+print("\n-----------\n")
+print("m2:")
+for row in m2:
+    print(row)
+print("\n-----------\n")
+wynik = [[0 for y in range(N)] for x in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            wynik[i][j] += m1[i][k] * m2[k][j]
 
 for row in wynik:
     print(row)
