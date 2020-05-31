9a03ead5c623364e1d351bfca84a33a9150543bb
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-05-31 12:19:09 +0200

    Create

--- zadania/03zaliczenie/example/main.py
+++ zadania/03zaliczenie/example/main.py
@@ -16,14 +16,32 @@ def usun():
             dane_programu.remove(wiersz)
             print("Dane: ", wiersz, " USUNIĘTE")
             usuniete = True
+            break
     if not usuniete:
         print("Brak danych dla", nazwisko, imie)
 
 
+def dodaj_wiersz():
+    nowy_wiersz = {}
+    nowy_wiersz["nazwisko"] = input('Podaj nazwisko: ')
+    nowy_wiersz["imie"]= input('Podaj imię: ')
+    nowy_wiersz["wiek"] = int(input('Podaj wiek: '))
+    nowy_wiersz["but"] = int(input('Podaj but: '))
+    nowy_wiersz["kolor_oczu"] = 1
+    dane_programu.append(nowy_wiersz)
+    print("Dodano nowe dane: ", nowy_wiersz)
+
+
+def zmien_wiek():
+    pass
+
+
 menu = {
     1: {"opis": "Wypisz dane na ekran", "funkcja_realizujaca": wypisz},
-    2: {"opis": "Usun dane", "funkcja_realizujaca": usun},
-    3: {"opis": "Sortuj po nazwisku", "funkcja_realizujaca": sortuj_dane_programu_po_nazwisku},
+    2: {"opis": "Utwórz wiersz", "funkcja_realizujaca": dodaj_wiersz},
+    3: {"opis": "Usun dane", "funkcja_realizujaca": usun},
+    4: {"opis": "Zmień wiek", "funkcja_realizujaca": zmien_wiek},
+    5: {"opis": "Sortuj po nazwisku", "funkcja_realizujaca": sortuj_dane_programu_po_nazwisku},
     9: {"opis": "Koniec programu", "funkcja_realizujaca": exit}
 }
 
@@ -55,7 +73,7 @@ if __name__ == "__main__":
         wypisz_menu()
         wybor = wybierz_opcje()
         if wybor > 0:
-            if wybor == 3:
+            if wybor == 5:
                 menu[wybor]["funkcja_realizujaca"](porownaj_wiersze)
             else:
                 menu[wybor]["funkcja_realizujaca"]()
