1500b8bbc92230fa72b1ab9437908a58c251b7fe
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-10-16 16:29:19 +0200

    Szyfr

---
+++ zadania/04pliki/Szyfr.py
@@ -0,0 +1,30 @@
+class Szyfr:
+
+    def __init__(self, nazwa_oryginalna, nazwa_szyfr):
+        self.oryginal = nazwa_oryginalna
+        self.szyfr = nazwa_szyfr
+
+    def szyfruj(self):
+        f_oryginal = open(self.oryginal, 'r')
+        lines = f_oryginal.readlines()
+        out_lines = []
+        for line in lines:
+            out_lines.append("")
+            for znak in line:
+                if znak == "\n":
+                    out_lines[len(out_lines) - 1] += znak
+                else:
+                    out_lines[len(out_lines)-1] += chr(ord(znak[0])+2)
+        f_oryginal.close()
+        print(out_lines)
+        f_szyfr = open(self.szyfr, 'w')
+        for line in out_lines:
+            f_szyfr.write(line)
+        f_szyfr.close()
+
+    def odszyfruj(self):
+        pass
+
+
+szyfr = Szyfr("plik.txt", "szyfr.txt")
+szyfr.szyfruj()
