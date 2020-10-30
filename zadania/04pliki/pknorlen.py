7ac804e5c401087df44140cf8e1aac3c958b2261
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-10-30 17:44:32 +0100

    100 data points

--- zadania/04pliki/pknorlen.py
+++ zadania/04pliki/pknorlen.py
@@ -12,6 +12,8 @@ with open('pknorlen_akcje.csv') as csvfile:
     for row in readCSV:
         kursy_lista.append({"data": row[0], "kurs_max": float(row[5]), "kurs_min": float(row[6])})
         counter += 1
+        if counter > 50:
+            break
 
 print(counter)
 print("Srednia: ", statistics.mean([k["kurs_max"] for k in kursy_lista]))
@@ -24,5 +26,6 @@ x_es = range(0,len(y_es))
 f_linear = interp1d(x_es, y_es, kind='linear')
 # xnew = np.arange(1, len(y_es), 0.1)
 
-plt.plot(x_es, f_linear(x_es), '-',)
+plt.plot(x_es, y_es, 'o',
+    x_es, f_linear(x_es), '-')
 plt.show()
