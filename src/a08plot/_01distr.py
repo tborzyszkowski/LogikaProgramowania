ce205f9d2f471794f4627c7cb225062745b774f0
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-10-30 17:35:37 +0100

    pkn plot

--- src/a08plot/_01distr.py
+++ src/a08plot/_01distr.py
@@ -15,7 +15,7 @@ s = np.random.normal(mu, sigma, 100000)
 print("Mean: ", abs(mu - np.mean(s)) < 0.01, np.mean(s))
 print("Var:  ", abs(sigma - np.std(s, ddof=1)) < 0.01, np.std(s, ddof=1))
 
-count, bins, ignored = plt.hist(s, 200)
+count, bins, ignored = plt.hist(s, 20)
 plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
                 np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
           linewidth=2, color='r')
--- zadania/04pliki/pknorlen.py
+++ zadania/04pliki/pknorlen.py
@@ -1,5 +1,6 @@
 import csv
 import statistics
+from matplotlib import pyplot as plt
 
 with open('pknorlen_akcje.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
@@ -15,3 +16,9 @@ print("Srednia: ", statistics.mean([k["kurs_max"] for k in kursy_lista]))
 print("Odch.Std:", statistics.stdev([k["kurs_max"] for k in kursy_lista]))
 print("Max: ", max([k["kurs_max"] for k in kursy_lista]))
 print("Min:", min([k["kurs_max"] for k in kursy_lista]))
+
+y_es = [k["kurs_max"] for k in kursy_lista]
+x_es = range(0,len(y_es))
+
+plt.plot(x_es, y_es, '.')
+plt.show()
