7e027e7a5df39cd62969b76b2c27df8a267f1d85
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2020-10-30 17:08:37 +0100

    pknorlen.py

---
+++ zadania/04pliki/pknorlen.py
@@ -0,0 +1,17 @@
+import csv
+import statistics
+
+with open('pknorlen_akcje.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    next(readCSV)
+    counter = 0
+    kursy_lista = []
+    for row in readCSV:
+        kursy_lista.append({"data": row[0], "kurs_max": float(row[5]), "kurs_min": float(row[6])})
+        counter += 1
+
+print(counter)
+print("Srednia: ", statistics.mean([k["kurs_max"] for k in kursy_lista]))
+print("Odch.Std:", statistics.stdev([k["kurs_max"] for k in kursy_lista]))
+print("Max: ", max([k["kurs_max"] for k in kursy_lista]))
+print("Min:", min([k["kurs_max"] for k in kursy_lista]))
