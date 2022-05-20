6c3e02719fe0d26d79d631d5e92563864e996604
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2022-05-20 18:42:51 +0200

    silnia

--- src/lab/lab02/z_1_silnia_v2.py
+++ src/lab/lab02/z_1_silnia_v2.py
@@ -1,5 +1,20 @@
 
-def silnia(n):
-    return 1
+# def silnia(n):
+#     licznik = 1
+#     wynik = 1
+#     while licznik <= n:
+#         wynik = wynik * licznik
+#         licznik = licznik + 1
+#     return wynik
 
-print(silnia(3))
+def silnia(n):
+    if n == 1:
+        return 1
+    else:
+        return n * silnia(n-1)
+
+print('1 == silnia(1):', 1 == silnia(1))
+print('2 == silnia(2):', 2 == silnia(2))
+print('6 == silnia(3):', 6 == silnia(3))
+print('24 == silnia(4):', 24 == silnia(4))
+print('120 == silnia(5):', 120 == silnia(5))
