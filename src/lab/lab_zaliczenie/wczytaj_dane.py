b36d6a5c675a5ff7c05bd114770dc8025a0eb7ad
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2024-10-20 17:55:54 +0200

    zmiany 2024.10.20

--- src/lab/lab_zaliczenie/wczytaj_dane.py
+++ src/lab/lab_zaliczenie/wczytaj_dane.py
@@ -9,3 +9,18 @@ def wczytaj_dane():
         for row in readCSV:
             result.append({"data": row[0], "kurs_max": float(row[5]), "kurs_min": float(row[6])})
     return result
+
+def wypisz_dane(dane):
+    for row in dane:
+        print(row)
+
+def policz_srednia(dane):
+    suma = 0
+    for row in dane:
+        suma += row["kurs_max"]
+    liczba_danych = len(dane)
+    return suma / liczba_danych
+
+if __name__ == '__main__':
+    dane = wczytaj_dane()
+    print("srednia: ", policz_srednia(dane))
