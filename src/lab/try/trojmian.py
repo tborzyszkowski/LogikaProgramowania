01bbc23f59ad36d950e936e91644701d02ca5046
Tomasz Borzyszkowski <t.borzyszkowski@gmail.com>
2025-06-07 09:21:36 +0200

    zmiany 2024.10.20

---
+++ src/lab/try/trojmian.py
@@ -0,0 +1,16 @@
+# a x^2 + b x + c = 0
+# miejsca zerowe
+
+a = 1
+b = 2
+c = 1
+
+if a != 0:
+    # ...
+    delta = b * b - 4 * a * c
+    if delta > 0:
+        # ...
+    else:
+        # ...
+else:
+    # a == 0
